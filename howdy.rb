bd4d7ba908b250aa8db092139ada46c8f7510def
呆萌的小辣条 <55776134+448267450@users.noreply.github.com>
2024-02-02 16:21:07 +0000

    initial commit

---
+++ howdy.rb
@@ -0,0 +1,12 @@
+my_string = "hello world"
+pp my_string
+
+require "./goodbye.rb"
+require "active_support/all"
+
+
+pp "What's your name?"
+
+their_name = gets.chomp
+
+pp "Hello, " + their_name + "!"
